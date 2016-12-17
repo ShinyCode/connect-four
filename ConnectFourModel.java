eb0ac2403c2a76e24f2c74c93311d7a4ad24fe70
S <s>
2016-12-17 11:23:28 -0800

    {"types":"C","runs":0}

--- ConnectFourModel.java
+++ ConnectFourModel.java
@@ -11,7 +11,23 @@ public class ConnectFourModel implements ConnectFourConstants {
 	}
 	
 	public String toString() {
-		
+		String result = "";
+		for(int r = 0; r < numRows(); r++) {
+			for(int c = 0; c < numCols(); c++) {
+				if(board[r][c] == PLAYER_ONE) result += PLAYER_ONE_SYMBOL;
+				else if (board[r][c] == PLAYER_TWO) result += PLAYER_TWO_SYMBOL;
+				else result += EMPTY_SYMBOL;
+			}
+		}
+	}
+	
+	private int numRows() {
+		return board.length;
+	}
+	
+	private int numCols() {
+		if(board.length > 0) return board[0].length;
+		return 0;
 	}
 	
 }
