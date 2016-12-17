ea544eba7c5966182eb4ee2a4e844f21efaf19a0
S <s>
2016-12-17 11:28:54 -0800

    {"types":"C","runs":0}

--- ConnectFourModel.java
+++ ConnectFourModel.java
@@ -12,13 +12,13 @@ public class ConnectFourModel implements ConnectFourConstants {
 	
 	public String toString() {
 		String result = "";
-		for(int r = 0; r < numRows(); r++) {
-			for(int c = 0; c < numCols(); c++) {
-				if(board[r][c] == PLAYER_ONE) result += PLAYER_ONE_SYMBOL;
-				else if (board[r][c] == PLAYER_TWO) result += PLAYER_TWO_SYMBOL;
+		for(int row = 0; row < numRows(); row++) {
+			for(int col = 0; col < numCols(); col++) {
+				if(board[row][col] == PLAYER_ONE) result += PLAYER_ONE_SYMBOL;
+				else if (board[row][col] == PLAYER_TWO) result += PLAYER_TWO_SYMBOL;
 				else result += EMPTY_SYMBOL;
 			}
-			if(r != numRows() - 1) result += ".%n";
+			if(row != numRows() - 1) result += ".%n";
 		}
 		return String.format(result);
 	}
@@ -30,6 +30,16 @@ public class ConnectFourModel implements ConnectFourConstants {
 	private int numCols() {
 		if(board.length > 0) return board[0].length;
 		return 0;
+	}
+	
+	public boolean makeMove(int player, int col) { // Returns whether the move was valid
+		if(col < 0 || col >= numCols() || isFull(col)) return false;
+		for(int row = numRows() - 1; row >= 0; row--)
+		return true;
+	}
+	
+	private boolean isFull(int col) {
+		return true;
 	}
 	
 }
