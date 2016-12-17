13478163d32a6baf0b8bcf355e3a392a22abd0d0
S <s>
2016-12-17 11:14:55 -0800

    {"types":"C","runs":0}

--- ConnectFourModel.java
+++ ConnectFourModel.java
@@ -1,7 +1,16 @@
 
 public class ConnectFourModel implements ConnectFourConstants {
+	int[][] board;
+	
+	public ConnectFourModel() {
+		board = new int[DEFAULT_ROWS][DEFAULT_COLS];
+	}
 	
 	public ConnectFourModel(int numRows, int numCols) {
+		board = new int[numRows][numCols];
+	}
+	
+	public String toString() {
 		
 	}
 	
