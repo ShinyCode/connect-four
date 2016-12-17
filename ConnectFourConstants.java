e3dff19550b188243cff28f37ea0fcd859a43faf
S <s>
2016-12-17 11:21:46 -0800

    {"types":"C","runs":0}

--- ConnectFourConstants.java
+++ ConnectFourConstants.java
@@ -3,4 +3,6 @@ public interface ConnectFourConstants {
 	public static final int DEFAULT_ROWS = 6;
 	public static final int DEFAULT_COLS = 7;
 	public static final int NUM_IN_A_ROW = 4;
+	public static final int PLAYER_ONE = 1;
+	public static final int PLAYER_TWO = -1;
 }
