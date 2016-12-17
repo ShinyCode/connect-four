baf2e69336ed96ad6d35cb302f8e6fb855d1367f
S <s>
2016-12-17 11:22:21 -0800

    {"types":"C","runs":0}

--- ConnectFourConstants.java
+++ ConnectFourConstants.java
@@ -5,4 +5,6 @@ public interface ConnectFourConstants {
 	public static final int NUM_IN_A_ROW = 4;
 	public static final int PLAYER_ONE = 1;
 	public static final int PLAYER_TWO = -1;
+	public static final char PLAYER_ONE_SYMBOL = 'O';
+	public static final char PLAYER_TWO_SYMBOL = 'X';
 }
