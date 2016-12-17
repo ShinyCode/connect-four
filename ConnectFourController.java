3c2ce34f0ecfd2d4d782ccb16795e876bbb6bcf8
S <s>
2016-12-17 12:28:56 -0800

    {"types":"C","runs":0}

--- ConnectFourController.java
+++ ConnectFourController.java
@@ -6,5 +6,21 @@ public class ConnectFourController extends ConsoleProgram implements ConnectFour
 	
 	public void run() {
 		model = new ConnectFourModel(DEFAULT_ROWS, DEFAULT_COLS);
+		int result = playGame();
+		determineWinner(result);
+	}
+	
+	private void playGame() {
+		
+	}
+	
+	private void determineWinner(int result) {
+		if(result == PLAYER_ONE) {
+			println("Player One was the winner!");
+		} else if(result == PLAYER_TWO) {
+			println("Player Two was the winner!");
+		} else {
+			println("The game ended in a draw.");
+		}
 	}
 }
