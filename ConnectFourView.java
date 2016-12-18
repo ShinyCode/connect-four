b6129a087792c0524cf69329b940f10d169debe9
S <s>
2016-12-17 20:27:38 -0800

    {"types":"C","runs":5}

--- ConnectFourView.java
+++ ConnectFourView.java
@@ -1,12 +1,16 @@
 import acm.graphics.*;
 
 public class ConnectFourView extends GCanvas implements ConnectFourConstants {
+	private int[][] board;
+	
 	public ConnectFourView() {
-		
+		board = new int[DEFAULT_ROWS][DEFAULT_COLS];
+		redraw();
 	}
 	
 	public ConnectFourView(int numRows, int numCols) {
-		
+		board = new int[numRows][numCols];
+		redraw();
 	}
 	
 	public void addMove(int player, int col) { // Blindly adds a move (no error-checking)
