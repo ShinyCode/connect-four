71c9330ffd31f83616d182abb906f309e5f92365
S <s>
2016-12-17 20:55:58 -0800

    {"types":"C","runs":15}

--- ConnectFourView.java
+++ ConnectFourView.java
@@ -29,12 +29,15 @@ public class ConnectFourView extends GCanvas implements ConnectFourConstants {
 	}
 	
 	private double calcCellWidth() {
+		double cellWidth = 0;
 		double availXSpace = getHeight() - (2 * BUTTON_MARGIN + BUTTON_HEIGHT) - BOARD_TOP_MARGIN - BOARD_BOTTOM_MARGIN;
 		double availYSpace = getWidth() - 2 * BOARD_SIDE_MARGIN;
 		int numRows = pieces.length;
 		int numCols = pieces[0].length;
 		if(((double)numRows / numCols) >= availYSpace / availXSpace) { // Too tall, use scale to availYSpace
-			
+			cellWidth = availYSpace / numRows;
+		} else {
+			cellWidth = availXSpace / numCols;
 		}
 	}
 	
