362f173acfa971a57c29a9336a0571317ddc4512
S <s>
2016-12-17 16:57:07 -0800

    {"types":"C","runs":5}

--- ConnectFourPlayer.java
+++ ConnectFourPlayer.java
@@ -1,4 +1,4 @@
 
 public abstract class ConnectFourPlayer {
-	public abstract int move(int[][] boardState);
+	public abstract int move(int[][] boardState); // Here, 1 is the player, -1 is the adversary
 }
