e1f873b72186efd2265ed83bf1f909c92d0cba47
S <s>
2016-12-18 21:53:54 -0800

    {"types":"C","runs":110}

--- SimpleAI.java
+++ SimpleAI.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class SimpleAI extends ConnectFourAI {
+
+public class SimpleAI extends ConnectFourAI implements ConnectFourConstants {
 
 	@Override
 	public int getMove(int[][] boardState) {
-		
+		ConnectFourModel model = new ConnectFourModel(boardState);
+		List<Integer> winningCols = new ArrayList<Integer>();
+		List<Integer> emptyCols = new ArrayList<Integer>();
+		for(int col = 0; col < model.numCols(); col++) {
+			if(model.colIsFull(col)) continue;
+			emptyCols.add(col);
+			model.makeMove(PLAYER_ONE, col);
+			if(model.checkWin() != NO_PLAYER) winningCols.add(col);
+			model.undoMove();
+		}
 		return 0;
 	}
 
