774e47ef2111077e28461a70757e14401ffaa7ae
S <s>
2016-12-19 11:01:23 -0800

    {"types":"C","runs":130}

--- MinimaxAI.java
+++ MinimaxAI.java
@@ -29,9 +29,13 @@ public class MinimaxAI extends ConnectFourAI implements ConnectFourConstants {
 		if(depth == 0) return eval(boardState);
 		if(player == PLAYER_ONE) {
 			double value = Double.NEGATIVE_INFINITY;
-			for(int col = 0; col < model.numCols; col++) {
-				
+			for(int col = 0; col < model.numCols(); col++) {
+				if(!model.makeMove(player, col)) continue;
+				double newValue = minimax(boardState, -player, depth);
+				if(newValue > value) value = newValue;
+				model.undoMove();
 			}
+			return value;
 		} else if (player == PLAYER_TWO) {
 			double value = Double.POSITIVE_INFINITY;
 			
