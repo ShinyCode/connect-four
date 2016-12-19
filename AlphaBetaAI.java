5fcae49efc5a13f9d39766c8b740e5497d3378cc
S <s>
2016-12-19 15:40:38 -0800

    {"types":"A","runs":206}

---
+++ AlphaBetaAI.java
@@ -0,0 +1,10 @@
+
+public class AlphaBetaAI extends ConnectFourAI implements ConnectFourConstants {
+
+	@Override
+	public int getMove(int[][] boardState) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
