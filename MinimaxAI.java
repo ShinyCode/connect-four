60f85c1c541dea63b7248ab6efa22a2b5aaf0e45
S <s>
2016-12-18 23:50:26 -0800

    {"types":"C","runs":129}

--- MinimaxAI.java
+++ MinimaxAI.java
@@ -3,7 +3,7 @@ public class MinimaxAI extends ConnectFourAI implements ConnectFourConstants {
 
 	@Override
 	public int getMove(int[][] boardState) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
