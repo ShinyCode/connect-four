022059fea29c081415ae4058b9ec6d5537c939ad
S <s>
2016-12-19 11:37:22 -0800

    {"types":"C","runs":136}

--- MinimaxAI.java
+++ MinimaxAI.java
@@ -60,7 +60,7 @@ public class MinimaxAI extends ConnectFourAI implements ConnectFourConstants {
 	}
 	
 	private double eval(ConnectFourModel model) {
-		return Double.POSITIVE_INFINITY;
+		return Double.NEGATIVE_INFINITY;
 	}
 
 }
