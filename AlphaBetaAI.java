6baa4ee16bb3a8ead3333d5f872e4b3b9c5c0659
S <s>
2016-12-19 15:40:50 -0800

    {"types":"C","runs":206}

--- AlphaBetaAI.java
+++ AlphaBetaAI.java
@@ -1,6 +1,11 @@
+import acm.util.RandomGenerator;
+
 
 public class AlphaBetaAI extends ConnectFourAI implements ConnectFourConstants {
-
+	private static final int DEFAULT_DEPTH = 3;
+	private int maxDepth;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
 	@Override
 	public int getMove(int[][] boardState) {
 		// TODO Auto-generated method stub
