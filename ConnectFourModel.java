afe8ca7c359429dfb92b012fabf37d32f778b05d
S <s>
2016-12-19 12:31:09 -0800

    {"types":"C","runs":148}

--- ConnectFourModel.java
+++ ConnectFourModel.java
@@ -213,7 +213,7 @@ public class ConnectFourModel implements ConnectFourConstants {
 		int count = 0;
 		for(int row = 0; row < numRows(); row++) {
 			for(int col = 0; col < numCols(); col++) {
-				
+				board[row][col]
 			}
 		}
 	}
