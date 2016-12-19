4e510c9cff24707c05b90626eba66b3056c0beef
S <s>
2016-12-18 18:29:35 -0800

    {"types":"C","runs":64}

--- ConnectFourView.java
+++ ConnectFourView.java
@@ -7,6 +7,7 @@ import acm.graphics.*;
 public class ConnectFourView extends GCanvas implements ConnectFourConstants, MouseListener {
 	private GOval[][] pieces;
 	private TouchButton[] buttons;
+	private GRect colorIndicator;
 	private boolean waitingForHumanMove = false;
 	private int humanMove = 0;
 	private double boardX = 0;
