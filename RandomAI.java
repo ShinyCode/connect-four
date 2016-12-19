d565ceb75d3637d5a2c01033d23e535576d70e23
S <s>
2016-12-18 20:40:04 -0800

    {"types":"C","runs":108}

--- RandomAI.java
+++ RandomAI.java
@@ -1,11 +1,13 @@
 
 public class RandomAI extends ConnectFourAI {
+	private int delay;
+	
 	public RandomAI() {
-		
+		delay = 0;
 	}
 	
 	public RandomAI(int delay) {
-		
+		this.delay = delay;
 	}
 	
 	@Override
