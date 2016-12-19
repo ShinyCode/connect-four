9e2a75f2e918d62d3b7aa0a178b1ffb1cd80fff5
S <s>
2016-12-18 23:51:42 -0800

    {"types":"C","runs":129}

--- MinimaxAI.java
+++ MinimaxAI.java
@@ -1,6 +1,16 @@
 
 public class MinimaxAI extends ConnectFourAI implements ConnectFourConstants {
-
+	private static final int DEFAULT_DEPTH = 5;
+	private int maxDepth;
+	
+	public MinimaxAI() {
+		maxDepth = DEFAULT_DEPTH;
+	}
+	
+	public MinimaxAI(int maxDepth) {
+		this.maxDepth = maxDepth;
+	}
+	
 	@Override
 	public int getMove(int[][] boardState) {
 		
