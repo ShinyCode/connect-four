cb27934dfb181bd460d293853a2e25f64e8fbe02
S <s>
2016-12-18 20:48:11 -0800

    {"types":"C","runs":108}

--- RandomAI.java
+++ RandomAI.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import acm.util.RandomGenerator;
+
 
 public class RandomAI extends ConnectFourAI implements ConnectFourConstants {
 	private int delay;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 	
 	public RandomAI() {
 		delay = 0;
@@ -17,12 +23,17 @@ public class RandomAI extends ConnectFourAI implements ConnectFourConstants {
 		} catch(InterruptedException ex) {}
 		int numRows = boardState.length;
 		int numCols = boardState[0].length;
+		List<Integer> emptyCols = new ArrayList<Integer>();
 		for(int col = 0; col < numCols; col++) {
 			for(int row = numRows - 1; row >= 0; row--) {
-				if
+				if(boardState[row][col] != PLAYER_ONE && boardState[row][col] != PLAYER_TWO) {
+					emptyCols.add(col);
+					break;
+				}
 			}
 		}
-		return NO_MOVE;
+		if(emptyCols.size() == 0) return NO_MOVE;
+		else return emptyCols.get(rgen.nextInt(emptyCols.size()));
 	}
 
 }
