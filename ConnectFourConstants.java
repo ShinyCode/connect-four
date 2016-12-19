b5259811cb1d74b81dedd39dca6a3c808263d421
S <s>
2016-12-19 14:30:13 -0800

    {"types":"C","runs":192}

--- ConnectFourConstants.java
+++ ConnectFourConstants.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 public interface ConnectFourConstants {
 	public static final int DEFAULT_ROWS = 6;
 	public static final int DEFAULT_COLS = 7;
-	public static final int NUM_IN_A_ROW = 4;
+	public static final int NUM_IN_A_ROW = 3;
 	public static final int NO_PLAYER = 0;
 	public static final int PLAYER_ONE = 1;
 	public static final int PLAYER_TWO = -1;
