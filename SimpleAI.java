5e63a5a318e402189095dbe367bfcfc5bdcab163
S <s>
2016-12-18 21:56:33 -0800

    {"types":"C","runs":110}

--- SimpleAI.java
+++ SimpleAI.java
@@ -5,10 +5,22 @@ import acm.util.RandomGenerator;
 
 
 public class SimpleAI extends ConnectFourAI implements ConnectFourConstants {
+	private int delay;
 	private RandomGenerator rgen = RandomGenerator.getInstance();
+	
+	public SimpleAI() {
+		delay = 0;
+	}
+	
+	public SimpleAI(int delay) {
+		this.delay = delay;
+	}
 	
 	@Override
 	public int getMove(int[][] boardState) {
+		try {
+			Thread.sleep(delay);
+		} catch(InterruptedException ex) {}
 		ConnectFourModel model = new ConnectFourModel(boardState);
 		List<Integer> winningCols = new ArrayList<Integer>();
 		List<Integer> emptyCols = new ArrayList<Integer>();
