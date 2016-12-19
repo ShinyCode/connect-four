03441109449b0923434e79b98ab262882f50622a
S <s>
2016-12-18 23:01:10 -0800

    {"types":"C","runs":119}

--- ConnectFourController.java
+++ ConnectFourController.java
@@ -15,7 +15,7 @@ public class ConnectFourController extends Program implements ConnectFourConstan
 	public void run() {
 		view.draw();
 		numValidMoves = 0;
-		int result = playGame(null, new SimpleAI(500));
+		int result = playGame(new SimpleAI(), new SimpleAI());
 		determineWinner(result);
 	}
 	
