36ce36f1afd273b68a2bb8710b378e49375eec5d
S <s>
2016-12-18 22:04:40 -0800

    {"types":"C","runs":116}

--- SimpleAI.java
+++ SimpleAI.java
@@ -28,6 +28,9 @@ public class SimpleAI extends ConnectFourAI implements ConnectFourConstants {
 		for(int col = 0; col < model.numCols(); col++) {
 			if(model.colIsFull(col)) continue;
 			emptyCols.add(col);
+			model.makeMove(PLAYER_TWO, col);
+			if(model.checkWin() != NO_PLAYER) dangerCols.add(col);
+			model.undoMove();
 			model.makeMove(PLAYER_ONE, col);
 			if(model.checkWin() != NO_PLAYER) winningCols.add(col);
 			model.undoMove();
