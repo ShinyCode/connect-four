e614930af770e93f45560ad441386a84c104ce47
S <s>
2016-12-20 09:49:35 -0800

    {"types":"C","runs":262}

--- ConnectFourAI.java
+++ ConnectFourAI.java
@@ -1,4 +1,17 @@
 
+/**
+ * Abstract class representing an AI for ConnectFour.
+ * 
+ * @author Mark Sabini
+ *
+ */
 public abstract class ConnectFourAI {
+	/**
+	 * Given the specified board state, returns the move that the AI believes
+	 * it should take.
+	 * 
+	 * @param boardState the current board state. PLAYER_ONE is always the AI, and PLAYER_TWO is always the adversary.
+	 * @return a move, where 0 <= move < numCols, or NO_MOVE
+	 */
 	public abstract int getMove(int[][] boardState); // Here, 1 is the player, -1 is the adversary
 }
